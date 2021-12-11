b151a528c119b61506ab5e9b87a9d2ff4cc48fd9
Idham <idham.m.irfani@gmail.com>
2021-12-12 01:40:24 +0700

    fix unit test failed

--- Modules/Game/Sources/Game/Data/UpdateFavoriteGameRepository.swift
+++ Modules/Game/Sources/Game/Data/UpdateFavoriteGameRepository.swift
@@ -30,7 +30,9 @@ Transformer.Domain == GameModel {
     }
     
     public func execute(request: Int?) -> AnyPublisher<GameModel, Error> {
-        return _localeDataSource.get(id: request ?? -1)
+        guard let request = request else { fatalError("Request cannot be empty") }
+        
+        return _localeDataSource.get(id: request)
             .map { _mapper.transformEntityToDomain(entity: $0) }
             .eraseToAnyPublisher()
     }
